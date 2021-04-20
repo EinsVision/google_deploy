572437fd8fbe51cfda25f36ca7f970edb1d47de4
Changdae <einsvision@gmail.com>
2021-04-20 13:48:34 +0900

    debugging for deploying

---
+++ keys.js
@@ -0,0 +1,7 @@
+// Google API Key https://developers.google.com/custom-search/v1/using_rest
+//export const API_KEY = 'AIzaSyDHTgGf9ACnqYPOxlgYAjdSlC3kC3MTBE4';
+export const API_KEY = 'AIzaSyBk0BtHNa9BL8jBwwAIi6nW7tQ1yR2AzEY';
+// Context Key https://cse.google.com/cse/create/new
+
+// export const CONTEXT_KEY = '6192e5ae6ee210000';
+export const CONTEXT_KEY = '25c33cf48ab940053';
